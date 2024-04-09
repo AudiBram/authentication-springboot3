ddbe238f46167971f53ee14ba385d11ca30120ad
Audi Bramastya <audibram14@gmail.com>
2024-04-10 02:22:28 +0700

    Implement spring security and configuring with JWT

---
+++ src/main/java/org/project/booksnetwork/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package org.project.booksnetwork.security;
+
+import lombok.RequiredArgsConstructor;
+import org.project.booksnetwork.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+}
