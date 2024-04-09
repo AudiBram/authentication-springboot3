2a35973fd2de3293f998f0dab78f0417063d7ac9
Audi Bramastya <audibram14@gmail.com>
2024-04-09 09:10:03 +0700

    Initial commit

---
+++ src/main/java/org/project/booksnetwork/BooksNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package org.project.booksnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksNetworkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksNetworkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/booksnetwork/BooksNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.booksnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksNetworkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
