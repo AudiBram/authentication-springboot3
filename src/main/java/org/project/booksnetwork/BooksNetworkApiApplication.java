4caf1a6843291a41e05609ac374289530d226f3b
Audi Bramastya <audibram14@gmail.com>
2024-04-09 13:09:21 +0700

    Define user and role entity

--- src/main/java/org/project/booksnetwork/BooksNetworkApiApplication.java
+++ src/main/java/org/project/booksnetwork/BooksNetworkApiApplication.java
@@ -2,8 +2,10 @@ package org.project.booksnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BooksNetworkApiApplication {
 
     public static void main(String[] args) {
