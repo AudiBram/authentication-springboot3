4a6ead6c28eb2e55969021d11740fe3f2849eb83
Audi Bramastya <audibram14@gmail.com>
2024-04-10 04:24:17 +0700

    Implement register and email service

---
+++ src/main/java/org/project/booksnetwork/auth/AuthenticationController.java
@@ -0,0 +1,27 @@
+package org.project.booksnetwork.auth;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Tag(name = "Authentication")
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    @ResponseStatus(ACCEPTED)
+    public ResponseEntity<?> register(@RequestBody @Valid RegistrationRequest request) throws MessagingException {
+        service.register(request);
+        return ResponseEntity.accepted().build();
+    }
+
+}
